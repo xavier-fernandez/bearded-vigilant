65bc4f2e417b1b4dc2911bf19d83c95d2677f7aa
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-04-28 23:05:29 +0200

    Add test to one single sensor entity insertion

--- mod-internal-sensors/src/androidTest/java/com/bearded/modules/sensor/internal/persistence/InternalSensorEntityFacadeTest.java
+++ mod-internal-sensors/src/androidTest/java/com/bearded/modules/sensor/internal/persistence/InternalSensorEntityFacadeTest.java
@@ -18,19 +18,31 @@
  */
 package com.bearded.modules.sensor.internal.persistence;
 
-import android.support.annotation.NonNull;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.annotation.Nullable;
 import android.test.InstrumentationTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
 
+import com.bearded.modules.sensor.internal.domain.InternalSensorEntity;
+import com.bearded.modules.sensor.internal.persistence.dao.DaoSession;
+
+import static android.content.Context.SENSOR_SERVICE;
 import static com.bearded.common.sensor.SensorType.LIGHT;
 
+/**
+ * Test the internal sensor entity facade using light sensors. We should consider that all modern
+ * Android phones have a light sensor integrated, this test class only works with physical phone.
+ * This is caused because it is forbidden to fake sensor values in Android, and the Sensor class
+ * is final.
+ */
 public class InternalSensorEntityFacadeTest extends InstrumentationTestCase {
 
-    @Nullable
     protected DatabaseConnector mDatabaseConnector;
-    @NonNull
     protected InternalSensorEntityFacade mSensorFacade;
+    protected SensorManager mSensorManager;
+    protected Sensor mLightSensor;
 
     /**
      * {@inheritDoc}
@@ -41,12 +53,36 @@ public class InternalSensorEntityFacadeTest extends InstrumentationTestCase {
         mDatabaseConnector = new DatabaseConnector(getInstrumentation().getContext(), "TEST_DB");
         mDatabaseConnector.cleanDatabase();
         mSensorFacade = new InternalSensorEntityFacade(LIGHT);
+        final Context context = getInstrumentation().getContext();
+        mSensorManager = (SensorManager) context.getSystemService(SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(LIGHT.getSensorId());
     }
 
+    /**
+     * Checks the conditions that all the class test are supposed to have before hand.
+     */
     @SmallTest
     public void testPreConditions() {
         assertNotNull(mDatabaseConnector);
         assertNotNull(mSensorFacade);
+        assertNotNull(mSensorManager);
+        assertNotNull(mLightSensor);
+        //Checks if the database is clean before the test starts.
+        final DaoSession session = mDatabaseConnector.getSession();
+        assertEquals(mSensorFacade.getAllSensorEntities(session).size(), 0);
+    }
+
+    @SmallTest
+    public void testOneObjectInsertion() {
+        testPreConditions();
+        //Inserts a single sensor inside the database.
+        final DaoSession session = mDatabaseConnector.getSession();
+        final InternalSensorEntity sensorEntity = mSensorFacade.getSensorEntity(session, mLightSensor);
+        assertNotNull(sensorEntity);
+        //Checks that the returned object belongs to the same sensor as the inserted sensor.
+        assertEquals(sensorEntity.getSensorName(), mLightSensor.getName());
+        //Checks that only one object was inserted.
+        assertEquals(mSensorFacade.getAllSensorEntities(session).size(), 1);
     }
 
     /**
@@ -55,6 +91,7 @@ public class InternalSensorEntityFacadeTest extends InstrumentationTestCase {
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
+        //Cleans the database before the next test starts.
         if (mDatabaseConnector != null) {
             mDatabaseConnector.cleanDatabase();
         }
