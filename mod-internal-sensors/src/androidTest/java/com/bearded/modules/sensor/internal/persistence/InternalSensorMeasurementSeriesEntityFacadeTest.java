2e9c4b5648bb59df988f8bc57182e29781e2b04a
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-05-02 13:48:02 +0200

    Add several test to the MeasurementSeriesEntityFacade

--- mod-internal-sensors/src/androidTest/java/com/bearded/modules/sensor/internal/persistence/InternalSensorMeasurementSeriesEntityFacadeTest.java
+++ mod-internal-sensors/src/androidTest/java/com/bearded/modules/sensor/internal/persistence/InternalSensorMeasurementSeriesEntityFacadeTest.java
@@ -1,15 +1,3 @@
-package com.bearded.modules.sensor.internal.persistence;
-
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.test.InstrumentationTestCase;
-import android.test.suitebuilder.annotation.SmallTest;
-
-import com.bearded.modules.sensor.internal.domain.InternalSensorEntity;
-import com.bearded.modules.sensor.internal.persistence.dao.DaoMaster;
-
-import static com.bearded.common.sensor.SensorType.LIGHT;
-
 /*
  * (C) Copyright 2015 Xavier Fernández Salas (xavier.fernandez.salas@gmail.com)
  *
@@ -28,12 +16,24 @@ import static com.bearded.common.sensor.SensorType.LIGHT;
  * Contributors:
  *      Xavier Fernández Salas (xavier.fernandez.salas@gmail.com)
  */
+
+package com.bearded.modules.sensor.internal.persistence;
+
+import android.test.InstrumentationTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import com.bearded.modules.sensor.internal.domain.InternalSensorEntity;
+import com.bearded.modules.sensor.internal.domain.InternalSensorMeasurementSeriesEntity;
+import com.bearded.modules.sensor.internal.persistence.dao.DaoSession;
+
 public class InternalSensorMeasurementSeriesEntityFacadeTest extends InstrumentationTestCase {
 
-    @Nullable
-    protected DatabaseConnector mDatabaseConnector;
-    @Nullable
-    protected InternalSensorEntityFacade mSensorFacade;
+    private static final byte NUMBER_OF_SENSOR_ENTITIES = 5;
+
+    private DatabaseConnector mDatabaseConnector;
+    private InternalSensorMeasurementSeriesEntityFacade mSeriesFacade;
+    private InternalSensorEntity[] mSensorEntity;
 
     /**
      * {@inheritDoc}
@@ -42,13 +42,120 @@ public class InternalSensorMeasurementSeriesEntityFacadeTest extends Instrumenta
     protected void setUp() throws Exception {
         super.setUp();
         mDatabaseConnector = new DatabaseConnector(getInstrumentation().getContext(), "TEST_DB");
-        mSensorFacade = new InternalSensorEntityFacade(LIGHT);
+        mSeriesFacade = new InternalSensorMeasurementSeriesEntityFacade();
+        generateSensorEntities();
+    }
+
+    private void generateSensorEntities() {
+        mSensorEntity = new InternalSensorEntity[NUMBER_OF_SENSOR_ENTITIES];
+        for (int i = 0; i < NUMBER_OF_SENSOR_ENTITIES; i++) {
+            mSensorEntity[i] = new InternalSensorEntity();
+            mSensorEntity[i].setSensorName(String.format("test sensor %d", i));
+            mSensorEntity[i].setSensorType(String.format("test type %d", i));
+            mSensorEntity[i].setSensorUnit(String.format("unit %d", i));
+            mDatabaseConnector.getSession().insert(mSensorEntity[i]);
+        }
     }
 
     @SmallTest
     public void testPreConditions() {
         assertNotNull(mDatabaseConnector);
-        assertNotNull(mSensorFacade);
+        assertNotNull(mSeriesFacade);
+        assertNotNull(mSensorEntity);
+    }
+
+    /**
+     * Test the proper measurement series creation using a given sensor.
+     *
+     * @see InternalSensorMeasurementSeriesEntityFacade#getActiveMeasurementSeries(DaoSession, InternalSensorEntity)
+     */
+    @MediumTest
+    public void testSensorEntityInsertion() {
+        testPreConditions();
+        final DaoSession session = mDatabaseConnector.getSession();
+        final InternalSensorMeasurementSeriesEntity series =
+                mSeriesFacade.getActiveMeasurementSeries(session, mSensorEntity[0]);
+        assertNotNull(series);
+        assertEquals(series.getInternalSensorEntity(), mSensorEntity[0]);
+    }
+
+    /**
+     * Test when a sensor is inserted twice, only one single measurement series is created.
+     *
+     * @see InternalSensorMeasurementSeriesEntityFacade#getActiveMeasurementSeries(DaoSession, InternalSensorEntity)
+     */
+    @MediumTest
+    public void testDuplicatedSensorEntityInsertion() {
+        testPreConditions();
+        final DaoSession session = mDatabaseConnector.getSession();
+        final InternalSensorMeasurementSeriesEntity series1 =
+                mSeriesFacade.getActiveMeasurementSeries(session, mSensorEntity[0]);
+        assertNotNull(series1);
+        final InternalSensorMeasurementSeriesEntity series2 =
+                mSeriesFacade.getActiveMeasurementSeries(session, mSensorEntity[0]);
+        assertNotNull(series2);
+        assertEquals(series1, series2);
+    }
+
+    /**
+     * Test the insertion of multiple different sensor entities.
+     *
+     * @see InternalSensorMeasurementSeriesEntityFacade#getActiveMeasurementSeries(DaoSession, InternalSensorEntity)
+     */
+    @MediumTest
+    public void testDifferentSensorEntitiesInsertion() {
+        testPreConditions();
+        final DaoSession session = mDatabaseConnector.getSession();
+        final InternalSensorMeasurementSeriesEntity[] series =
+                new InternalSensorMeasurementSeriesEntity[NUMBER_OF_SENSOR_ENTITIES];
+        for (int i = 0; i < NUMBER_OF_SENSOR_ENTITIES; i++) {
+            series[i] = mSeriesFacade.getActiveMeasurementSeries(session, mSensorEntity[i]);
+            assertEquals(series[i].getInternalSensorEntity(), mSensorEntity[i]);
+        }
+    }
+
+    /**
+     * Test the insertion of multiple different sensor entities.
+     *
+     * @see InternalSensorMeasurementSeriesEntityFacade#getActiveMeasurementSeries(DaoSession, InternalSensorEntity)
+     */
+    @MediumTest
+    public void testDuplicatedDifferentSensorEntitiesInsertion() {
+        testPreConditions();
+        final DaoSession session = mDatabaseConnector.getSession();
+        final InternalSensorMeasurementSeriesEntity[] series =
+                new InternalSensorMeasurementSeriesEntity[NUMBER_OF_SENSOR_ENTITIES];
+        for (int i = 0; i < NUMBER_OF_SENSOR_ENTITIES; i++) {
+            series[i] = mSeriesFacade.getActiveMeasurementSeries(session, mSensorEntity[i]);
+            assertEquals(series[i].getInternalSensorEntity(), mSensorEntity[i]);
+        }
+        // Retrieves an the inserted sensors again, and checks if the retrieved elements
+        // are the same as in the first insertion.
+        for (int i = 0; i < NUMBER_OF_SENSOR_ENTITIES; i++) {
+            final InternalSensorMeasurementSeriesEntity duplicatedSeries =
+                    mSeriesFacade.getActiveMeasurementSeries(session, mSensorEntity[i]);
+            assertEquals(series[i], duplicatedSeries);
+        }
+    }
+
+    /**
+     * Test that all the closed elements from a sensor are retrieved properly.
+     */
+    @MediumTest
+    public void testGetAllClosedMeasurementSeriesFromSensor() {
+        testPreConditions();
+        final DaoSession session = mDatabaseConnector.getSession();
+        //Test if the sensor facade is empty.
+        assertEquals(mSeriesFacade.getAllClosedMeasurementSeriesFromSensor(
+                session, mSensorEntity[0]).size(), 0);
+        for (int i = 0; i < NUMBER_OF_SENSOR_ENTITIES; i++) {
+            mSeriesFacade.getActiveMeasurementSeries(session, mSensorEntity[i]);
+            assertEquals(0, mSeriesFacade.getAllClosedMeasurementSeriesFromSensor(session, mSensorEntity[i]).size());
+        }
+        mSeriesFacade.updateAllMeasurementSeriesEndTimestamp(session);
+        for (int i = 0; i < NUMBER_OF_SENSOR_ENTITIES; i++) {
+            assertEquals(1, mSeriesFacade.getAllClosedMeasurementSeriesFromSensor(session, mSensorEntity[i]).size());
+        }
     }
 
     /**
