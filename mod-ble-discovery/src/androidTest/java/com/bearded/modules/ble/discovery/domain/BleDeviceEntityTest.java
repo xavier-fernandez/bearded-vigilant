311537e8c2f2bd973cf35bfd10860ca96989f51a
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-05-12 17:54:02 +0200

    Test the BleDeviceEntity

--- mod-ble-discovery/src/androidTest/java/com/bearded/modules/ble/discovery/domain/BleDeviceEntityTest.java
+++ mod-ble-discovery/src/androidTest/java/com/bearded/modules/ble/discovery/domain/BleDeviceEntityTest.java
@@ -1,22 +1,60 @@
 package com.bearded.modules.ble.discovery.domain;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+
 import com.bearded.modules.ble.discovery.persistence.dao.BleDeviceEntityDao;
+import com.google.gson.JsonObject;
 
 import de.greenrobot.dao.test.AbstractDaoTestLongPk;
+
+import static com.bearded.modules.ble.discovery.persistence.dao.BleDeviceEntityDao.Properties.AdvertiseName;
+import static com.bearded.modules.ble.discovery.persistence.dao.BleDeviceEntityDao.Properties.DeviceAddress;
 
 public class BleDeviceEntityTest extends AbstractDaoTestLongPk<BleDeviceEntityDao, BleDeviceEntity> {
 
     private static final String TEST_DEVICE_ADDRESS = "AA:BB:CC:DD:EE:FF";
+    private static final String TEST_ADVERTISE_NAME = "Advertise Name";
 
     public BleDeviceEntityTest() {
         super(BleDeviceEntityDao.class);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    protected BleDeviceEntity createEntity(Long key) {
+    @NonNull
+    protected BleDeviceEntity createEntity(@Nullable final Long key) {
         final BleDeviceEntity entity = new BleDeviceEntity();
         entity.setId(key);
         entity.setDeviceAddress(TEST_DEVICE_ADDRESS);
+        entity.setAdvertiseName(TEST_ADVERTISE_NAME);
         return entity;
+    }
+    
+    /**
+     * Test if the entity attributes have been inserted properly.
+     */
+    @MediumTest
+    public void testEntityAttributes() {
+        final long deviceId = 1l;
+        final BleDeviceEntity entity = this.createEntity(deviceId);
+        assertEquals(entity.getDeviceAddress(), TEST_DEVICE_ADDRESS);
+        assertEquals(entity.getAdvertiseName(), TEST_ADVERTISE_NAME);
+        assertEquals(entity.getId().longValue(), deviceId);
+    }
+
+    /**
+     * Test the @see InternalSensorEntity#toJson
+     */
+    @SmallTest
+    public void testToJson() {
+        final BleDeviceEntity entity = this.createEntity(1l);
+        final JsonObject jsonObject = entity.toJsonObject();
+        assertEquals(jsonObject.get(DeviceAddress.name).getAsString(), TEST_DEVICE_ADDRESS);
+        assertEquals(jsonObject.get(AdvertiseName.name).getAsString(), TEST_ADVERTISE_NAME);
     }
 }
