c9e41e304081768d60dca3d0070f8ec76dc83aeb
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-05-12 23:05:45 +0200

    Add test to the BleEentSeriesEntity

--- mod-ble-discovery/src/androidTest/java/com/bearded/modules/ble/discovery/domain/BleEventSeriesEntityTest.java
+++ mod-ble-discovery/src/androidTest/java/com/bearded/modules/ble/discovery/domain/BleEventSeriesEntityTest.java
@@ -1,37 +1,65 @@
 package com.bearded.modules.ble.discovery.domain;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
 
 import com.bearded.modules.ble.discovery.persistence.dao.BleEventSeriesEntityDao;
+import com.google.gson.JsonObject;
+
+import org.joda.time.DateTime;
 
 import de.greenrobot.dao.test.AbstractDaoTestLongPk;
 
 import static com.bearded.common.time.TimeUtils.timestampToISOString;
+import static com.bearded.modules.ble.discovery.persistence.dao.BleEventSeriesEntityDao.Properties.EndTimestamp;
+import static com.bearded.modules.ble.discovery.persistence.dao.BleEventSeriesEntityDao.Properties.StartTimestamp;
 
 public class BleEventSeriesEntityTest extends AbstractDaoTestLongPk<BleEventSeriesEntityDao, BleEventSeriesEntity> {
 
-    private static final short ONE_SECOND_IN_MILLISECONDS = 1000;
-    private static final int EVENT_SERIES_TEST_DURATION_SECONDS = 10 * ONE_SECOND_IN_MILLISECONDS;
+    @NonNull
+    private static final DateTime TEST_END_TIME = DateTime.now();
+    @NonNull
+    private static final String TEST_END_TIMESTAMP = timestampToISOString(TEST_END_TIME);
+    @NonNull
+    private static final String TEST_START_TIMESTAMP = timestampToISOString(TEST_END_TIME.minusMinutes(1));
 
     public BleEventSeriesEntityTest() {
         super(BleEventSeriesEntityDao.class);
     }
 
     /**
-     * Returns a device entity using a start timestamp from ten seconds before this method calling.
-     *
-     * @return {@link java.lang.String} with the start timestamp following the ISO 8601 convention.
+     * {@inheritDoc}
      */
-    @NonNull
-    private static String getIsoStartTimestamp() {
-        return timestampToISOString(System.currentTimeMillis() - EVENT_SERIES_TEST_DURATION_SECONDS);
-    }
-
     @Override
-    protected BleEventSeriesEntity createEntity(Long key) {
+    @NonNull
+    protected BleEventSeriesEntity createEntity(@Nullable final Long key) {
         final BleEventSeriesEntity entity = new BleEventSeriesEntity();
         entity.setId(key);
-        entity.setStartTimestamp(getIsoStartTimestamp());
+        entity.setEndTimestamp(TEST_END_TIMESTAMP);
+        entity.setStartTimestamp(TEST_START_TIMESTAMP);
         return entity;
+    }
+
+    /**
+     * Test if the entity attributes have been set properly.
+     */
+    @MediumTest
+    public void testEntityAttributes() {
+        final BleEventSeriesEntity eventEntity = this.createEntity(1l);
+        assertEquals(eventEntity.getStartTimestamp(), TEST_START_TIMESTAMP);
+        assertEquals(eventEntity.getEndTimestamp(), TEST_END_TIMESTAMP);
+    }
+    
+    /**
+     * Test the @see BleEventSeriesEntity#toJson
+     */
+    @SmallTest
+    public void testToJson() {
+        final BleEventSeriesEntity entity = this.createEntity(1l);
+        final JsonObject jsonObject = entity.toJsonObject();
+        assertEquals(jsonObject.get(StartTimestamp.name).getAsString(), TEST_START_TIMESTAMP);
+        assertEquals(jsonObject.get(EndTimestamp.name).getAsString(), TEST_END_TIMESTAMP);
     }
 }
