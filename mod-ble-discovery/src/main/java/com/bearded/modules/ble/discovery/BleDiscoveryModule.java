ba051cd25519f389c4c64ff893b668196f7a09f3
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2015-06-15 18:25:25 +0200

    Implement BleDiscoveryModule

--- mod-ble-discovery/src/main/java/com/bearded/modules/ble/discovery/BleDiscoveryModule.java
+++ mod-ble-discovery/src/main/java/com/bearded/modules/ble/discovery/BleDiscoveryModule.java
@@ -22,9 +22,12 @@ package com.bearded.modules.ble.discovery;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.bearded.common.modules.AbstractCloudModule;
 import com.bearded.modules.ble.discovery.persistence.BleDiscoveryDatabaseFacade;
+import com.bearded.modules.ble.discovery.persistence.cloud.BluetoothDiscoveryCloudUploader;
+import com.google.gson.JsonObject;
 import com.sensirion.libble.BleManager;
 import com.sensirion.libble.devices.BleDevice;
 import com.sensirion.libble.listeners.devices.DeviceStateListener;
@@ -32,16 +35,30 @@ import com.sensirion.libble.listeners.devices.ScanListener;
 
 import org.joda.time.DateTime;
 
+@SuppressWarnings("unused")
 public class BleDiscoveryModule extends AbstractCloudModule implements ScanListener, DeviceStateListener {
 
+    @NonNull
     private static final String TAG = BleDiscoveryModule.class.getSimpleName();
     private static final int BLE_DISCOVERY_MODULE_VERSION = 1;
+    private static final int DATA_BIN_TIME_MS = 5 * 1000; // 5 SECONDS
     @NonNull
     private final BleDiscoveryDatabaseFacade mDatabaseFacade;
+    @NonNull
+    private final BluetoothDiscoveryCloudUploader mCloudUploader;
+    private byte mConsecutiveTimeouts = 0;
+    @Nullable
+    private DateTime mLastDataReceived;
+    @Nullable
+    private DateTime mLastDataSentToCloud;
 
     public BleDiscoveryModule(@NonNull final Context context) {
         super(context);
-        mDatabaseFacade = new BleDiscoveryDatabaseFacade();
+        mDatabaseFacade = new BleDiscoveryDatabaseFacade(context, DATA_BIN_TIME_MS);
+        mCloudUploader = new BluetoothDiscoveryCloudUploader();
+        BleManager.getInstance().registerNotificationListener(this);
+        BleManager.getInstance().startScanning();
+        Log.i(TAG, "BleDiscoveryModule -> Discovery Module have been initialized.");
     }
 
     /**
@@ -66,8 +83,7 @@ public class BleDiscoveryModule extends AbstractCloudModule implements ScanListe
      */
     @Override
     public boolean isModuleEnabled() {
-        //TODO: Implement
-        return false;
+        return BleManager.getInstance().isBluetoothEnabled();
     }
 
     /**
@@ -76,33 +92,60 @@ public class BleDiscoveryModule extends AbstractCloudModule implements ScanListe
     @Nullable
     @Override
     public DateTime getLastSensorDataReceived() {
-        //TODO: Implement
-        return null;
+        return mLastDataReceived;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Nullable
     @Override
     public DateTime getLastCloudUploadTime() {
-        return null;
+        return mLastDataSentToCloud;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void pushCloudDataToTheCloud() {
-        //TODO: Implement
+        final JsonObject jsonObject = mDatabaseFacade.getBleDiscoveryDataJson(getDeviceMetadataJson());
+        if (jsonObject == null) {
+            Log.w(TAG, "pushCloudDataToTheCloud -> No data to upload.");
+        } else if (getLastSensorDataReceived() == null) {
+            Log.w(TAG, "pushCloudDataToCloud -> No data have been received yet.");
+            BleManager.getInstance().registerNotificationListener(this);
+        } else {
+            Log.d(TAG, "pushCloudDataToCloud -> Uploading cloud data.");
+            mCloudUploader.uploadBluetoothDiscoveryData(jsonObject.toString(), this);
+        }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onScanStateChanged(final boolean isScanEnabled) {
         if (!isScanEnabled) {
             BleManager.getInstance().startScanning();
+            Log.d(TAG, "onScanStateChanged -> Scan disabled, ");
+            for (final BleDevice device : BleManager.getInstance().getDiscoveredBleDevices()) {
+                this.onDeviceDiscovered(device);
+            }
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onDeviceConnected(@NonNull final BleDevice device) {
         // Do nothing
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onDeviceDisconnected(@NonNull final BleDevice device) {
         // Do nothing
@@ -114,7 +157,9 @@ public class BleDiscoveryModule extends AbstractCloudModule implements ScanListe
      */
     @Override
     public void onDeviceDiscovered(@NonNull final BleDevice device) {
-        //TODO: Implement
+        Log.i(TAG, String.format("onDeviceDiscovered -> Discovered device %s with RSSI %d.", device.getAddress(), device.getRSSI()));
+        mDatabaseFacade.insertBleEvent(device);
+        mLastDataReceived = DateTime.now();
     }
 
     /**
@@ -130,6 +175,25 @@ public class BleDiscoveryModule extends AbstractCloudModule implements ScanListe
      */
     @Override
     public void onUploadCompleted(final int code) {
-        // TODO: Implement
+        Log.d(TAG, String.format("onUploadCompleted -> Upload completed with code: %d", code));
+        mDatabaseFacade.purgeAllUploadedBleEvents();
+        mLastDataSentToCloud = DateTime.now();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onUploadFailure(@Nullable final String message) {
+        Log.d(TAG, String.format("onUploadFailure with message: %s", message));
+        if (message != null && message.startsWith("timeout")) {
+            synchronized (this) {
+                if (mConsecutiveTimeouts++ > 3) {
+                    mConsecutiveTimeouts = 0;
+                    Log.w(TAG, "onUploadFailure -> Purging database, file is to big for sending it completely to the cloud.");
+                    mDatabaseFacade.purgeAllUploadedBleEvents();
+                }
+            }
+        }
     }
 }
